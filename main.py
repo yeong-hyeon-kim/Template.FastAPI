7d5694a46944c55812b87e38ac038b095423de85
INA <codechemi@gmail.com>
2022-05-09 11:46:26 +0900

    Init Template!

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
+from sre_parse import State
+import string
+from sys import intern
+from tokenize import String
 from typing import Optional
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Note(BaseModel):
+    title : str
+    content: str
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
+# API
+@app.post("/notes/{note_id}")
+async def update_note(note_id: int, note_element: str):
+    return{"note_id": note_id, "note_title" : Note.title, "note_content" : Note.contentq}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/notes/{note_id}")
+async def read_note(note_id: int, note_title: str):
+    return{"note_id": note_id, "note_title" : note_title}
+
+@app.put("/notes/{note_id}")
+async def update_note(note_id: int, note_element: str):
+    return{"note_id": note_id, "note_title" : Note.title, "note_content" : Note.contentq}
+
+@app.delete("/notes/{note_id}")
+async def delete_note(note_id: int):
+    return{"note_id": note_id}

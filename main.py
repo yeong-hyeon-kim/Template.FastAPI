1a701dcd2b0c5c754db433a31cc1f5ac6269d776
INA <codechemi@gmail.com>
2022-05-09 14:43:44 +0900

    Refactor : Edit Function

--- main.py
+++ main.py
@@ -10,23 +10,25 @@ app = FastAPI()
 class Note(BaseModel):
     title : str
     content: str
+    is_offer: Optional[bool] = None
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
+
 # API
 @app.post("/notes/{note_id}")
-async def update_note(note_id: int, note_element: str):
-    return{"note_id": note_id, "note_title" : Note.title, "note_content" : Note.contentq}
+async def update_note(note_id: int, note_title: str, note_content: str):
+    return{"note_id": note_id, "note_title" : note_title, "note_content" : note_content}
 
 @app.get("/notes/{note_id}")
-async def read_note(note_id: int, note_title: str):
-    return{"note_id": note_id, "note_title" : note_title}
+async def read_note(note_id: int):
+    return{"note_id": note_id}
 
 @app.put("/notes/{note_id}")
-async def update_note(note_id: int, note_element: str):
-    return{"note_id": note_id, "note_title" : Note.title, "note_content" : Note.contentq}
+async def update_note(note_id: int, note_element: Optional[str] = None):
+    return{"note_id": note_id, "note_element" : note_element}
 
 @app.delete("/notes/{note_id}")
 async def delete_note(note_id: int):
